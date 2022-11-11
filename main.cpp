c446cf9bdb6ac888c094f922eae6112207388a5e
s5363612 <115482723+s5363612@users.noreply.github.com>
2022-11-11 09:39:15 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   cout << "Hello, World!" << endl;
-   return 0;
+int main(){
+  float a, b, c;
+
+  cin >> a >> b >> c;
+
+  cout<<0.5*a*b<<endl;
+  cout<<a*a<<endl;
+  cout<<a*b<<endl;
+  cout<<0.5*(a+b)*c<<endl;
+  return 0;
 }
